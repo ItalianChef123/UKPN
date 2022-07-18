bec76f32003fb4d84413c27576134b7b5338093b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-18 15:31:19 +0100

    Completed Challenge 5

--- Answers.py
+++ Answers.py
@@ -8,7 +8,14 @@ import matplotlib.pyplot as plt
 # The two best programs to open csv files are microsoft excel and scribble maps
 
 #Challenge 3
-pd.read_csv
-np.genfromtxt()
+#pd.read_csv
+#np.genfromtxt()
 
 #Challenge 4
+
+#Challenge 5
+import os
+fname= "data\\test_data_for_oli.csv"
+fname2=os.path.join(os.getcwd(), "data", "test_data_for_oli.csv")
+print(fname)
+print(fname2)
